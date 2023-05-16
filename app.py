349c968f61b72bfb9f43f88147810e9a7b6731b2
onyu3825 <126893825+onyu3825@users.noreply.github.com>
2023-05-16 12:46:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-df = pd.DataFrame(np.rnadom.randn(500,2) / [50, 50] + [37.76, -122.4]),columns=['lat', 'lon'])
+df = pd.DataFrame(np.radom.randn(500,2) / [50, 50] + [37.76, -122.4]),columns=['lat', 'lon'])
 st.map(df)
 
 #x = st.slider('Select a value')
